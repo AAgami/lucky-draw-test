0d4edf161c33738ece3076990d2fbcd5e2fc3e57
hel00 <48651886+AAgami@users.noreply.github.com>
2025-04-02 11:08:30 +0900

    fix: fetch 주소 수정

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 function getRandomMessage() {
-    fetch("http://127.0.0.1:5500")  // 백엔드 API 호출
+    fetch("http://localhost:8080/api/random-message")  // 백엔드 API 호출
         .then(response => response.text())  // 응답 데이터를 텍스트로 변환
         .then(data => {
+            console.log(data)
             document.getElementById("message").textContent = data;  // 화면에 메시지 표시
         })
         .catch(error => {
